c91d88cf65dae95b193b80ed8a47ccfd652958ad
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-07 19:43:33 -0700

    initial backbone refactor of messages models and views

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,125 +1,182 @@
-// YOUR CODE HERE:
-var rooms = {};
-var user = {
-  // name: name,
-  // friends: []
-};
 
+/////////////////////////////
+///Backbone Implementation///
+/////////////////////////////
 
-var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
-  init: function() { app.fetch(); },
-  send: function( message ) { post(message); },
-  fetch: function() { get( parseRooms ); },
-  addMessage: function( message ) { $('#chats').append( message ) },
-  clearMessages: function() { $('#chats').empty() },
-  addRoom: function( roomName ) { $('#roomSelect').append( toHTML( roomName, 'room' ) ) },
-};
+var Message = Backbone.Model.extend({
+  
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  defaults: {}
 
+});
 
-var post = function(message){
-  $.ajax({
-    url: app.server,
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+var Messages = Backbone.Collection.extend({
+  
+  model: Message,
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  
+  loadMsgs: function(){
+    this.fetch({ data: { order: '-createdAt' } });
+  },
 
-var get = function(fn, room){
+  parse: function(response, options){
+    var results = [];
+    _.each(response.results, function(message){
+      results.push(message);
+    });
 
-  $.ajax({
-    url: app.server,
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message received');
-      console.log(data);
-      fn(data, room);
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to receive message');
-    }
-  });
-};
-
-var toHTML = function(str, cssClass){
-  //deal with escaping
-  if(str){
-    var lt = /</g,
-    gt = />/g,
-    ap = /'/g,
-    ic = /"/g;
-    str = str.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
-    return "<div class=" + cssClass + ">" + str + "</div>";
+    return results;
   }
-}
 
-//Populate list of rooms
+});
 
-var addRooms = function(rooms){
-  _.each(rooms, function(room){
-    app.addRoom(room);
-  });
-};
+var MessageView = Backbone.View.extend({
 
-var parseRooms = function(response){
-  var data = response.results;
-  _.each(data, function(datum){
-      rooms[datum.roomname] = datum.roomname;
-  });
+  template: _.template('<div class="chat" data-id="<%- objectId %>">\
+                        <div class="user" data-id="<%- username %>">\
+                        <div class="text" data-id="<%- text %>">\
+                        </div>'),
 
-  addRooms(rooms);
-};
+  render: function(){
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+
+});
 
 
-//Display all messages in the selected room
-//All messages have username and message (and room)
+var MessagesView = Backbone.View.extend({
+  
+  initialize: function(){
+    this.collection.on('sync', this.render, this);
+    this.onscreenmessages = {};
+  },
+  
+  render: function(){
+    this.collection.forEach(this.renderMessage, this);
+  },
 
-var parseMessages = function(response, room){
-  var data = response.results;
-  console.log("parsethis", this)
-  _.each(data, function(datum){
-    if(datum.roomname === room){
-      createMessage(datum.text, datum.username);
+  renderMessage: function(message){
+    if ( !this.onscreenmessages[message.get('objectId')] ){
+      var messageView = new MessageView({ model: message });
     }
-  });
-}
+  }
 
-var createMessage = function( message, username ){
-  var $div = '<div class="message">' + toHTML(username, 'username') + toHTML(message, 'chat') + "</div>";
-  app.addMessage($div);
-}
-
-
-app.init();
+})
 
 
 
-//Click on room
-//Need to keep list of friends
-//Need to bold class messages from friends
-//Need place to post new messages
-//
-//Need to regularly update messages
-//
-
-
-//Option one:
-//parseMessages provides list of rooms
-//gives us messages for each room
-//associates username for each message
-//
-//
-//Option two:
-//parseroom function, parsemessage function
 
 
 
+
+
+/////////////////////////////
+////Jquery Implementation////
+/////////////////////////////
+
+
+
+
+// // YOUR CODE HERE:
+// var rooms = {};
+// var user = {
+//   // name: name,
+//   // friends: []
+// };
+
+
+// var app = {
+//   server: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+//   init: function() { app.fetch(); },
+//   send: function( message ) { post(message); },
+//   fetch: function() { get( parseRooms ); },
+//   addMessage: function( message ) { $('#chats').append( message ) },
+//   clearMessages: function() { $('#chats').empty() },
+//   addRoom: function( roomName ) { $('#roomSelect').append( toHTML( roomName, 'room' ) ) },
+// };
+
+
+// var post = function(message){
+//   $.ajax({
+//     url: app.server,
+//     type: 'POST',
+//     data: JSON.stringify(message),
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('chatterbox: Message sent');
+//     },
+//     error: function (data) {
+//       console.error('chatterbox: Failed to send message');
+//     }
+//   });
+// };
+
+// var get = function(fn, room){
+
+//   $.ajax({
+//     url: app.server,
+//     type: 'GET',
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('chatterbox: Message received');
+//       console.log(data);
+//       fn(data, room);
+//     },
+//     error: function (data) {
+//       console.error('chatterbox: Failed to receive message');
+//     }
+//   });
+// };
+
+// var toHTML = function(str, cssClass){
+//   //deal with escaping
+//   if(str){
+//     var lt = /</g,
+//     gt = />/g,
+//     ap = /'/g,
+//     ic = /"/g;
+//     str = str.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
+//     return "<div class=" + cssClass + ">" + str + "</div>";
+//   }
+// }
+
+// //Populate list of rooms
+
+// var addRooms = function(rooms){
+//   _.each(rooms, function(room){
+//     app.addRoom(room);
+//   });
+// };
+
+// var parseRooms = function(response){
+//   var data = response.results;
+//   _.each(data, function(datum){
+//       rooms[datum.roomname] = datum.roomname;
+//   });
+
+//   addRooms(rooms);
+// };
+
+
+// //Display all messages in the selected room
+// //All messages have username and message (and room)
+
+// var parseMessages = function(response, room){
+//   var data = response.results;
+//   console.log("parsethis", this)
+//   _.each(data, function(datum){
+//     if(datum.roomname === room){
+//       createMessage(datum.text, datum.username);
+//     }
+//   });
+// }
+
+// var createMessage = function( message, username ){
+//   var $div = '<div class="message">' + toHTML(username, 'username') + toHTML(message, 'chat') + "</div>";
+//   app.addMessage($div);
+// }
+
+
+// // app.init();
 
--- client/scripts/interaction.js
+++ client/scripts/interaction.js
@@ -1,24 +1,24 @@
-$(document).ready( function () {
+// $(document).ready( function () {
 
-  //Need to provide place to select room
-  $('body').on('click', '.room', function(){
-    var room = $(this).text();
-    $('#room').text(room);
-    app.clearMessages();
-    get(parseMessages, room);
-  });
+//   //Need to provide place to select room
+//   $('body').on('click', '.room', function(){
+//     var room = $(this).text();
+//     $('#room').text(room);
+//     app.clearMessages();
+//     get(parseMessages, room);
+//   });
 
-  $('button').click(function(){
-    event.preventDefault();
-    var message = {
-      'username': $('#name').val(),
-      'text': $('#msg').val(),
-      'roomname': $('#room').val()
-    }
-    app.send(message);
-    app.clearMessages();
-    get(parseMessages, $('#room').val());
-  });
+//   $('button').click(function(){
+//     event.preventDefault();
+//     var message = {
+//       'username': $('#name').val(),
+//       'text': $('#msg').val(),
+//       'roomname': $('#room').val()
+//     }
+//     app.send(message);
+//     app.clearMessages();
+//     get(parseMessages, $('#room').val());
+//   });
 
 
-});
+// });
